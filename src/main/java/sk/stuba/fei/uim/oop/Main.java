8f41410f717b9c6d0e9a88afa7e289b7d1cbbc63
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-17 22:06:33 +0100

    map

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -4,38 +4,45 @@ package sk.stuba.fei.uim.oop;
 import sk.stuba.fei.uim.oop.zvierata.*;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Zviera> zveri = new ArrayList<>();
-        zveri.add(new Pes("Dunco"));
-        zveri.add(new Macka("Murko"));
+        List<Zviera> zveri = new ArrayList<>(List.of(new Pes("Dunco"), new Macka("Murko")));
 
-        for (var zver : zveri){
-            System.out.println("pred ");
-            try {
-                System.out.println("pred try ");
-                kontrola(zver);
-                System.out.println("po try ");
-            } catch (NotPesException e) {
-                System.out.println("catch pes ");
-                System.out.println(e.getMessage());
-            } catch (NotMackaException e) {
-                System.out.println("catch macka ");
-                System.out.println(e.getMessage());
-            }finally {
-                System.out.println("finally ");
-            }
-            System.out.println("po ");
-            System.out.println("-----");
+        Map<String, String> hlavneMesta = new HashMap<>();
+        hlavneMesta.put("Slovensko", "Bratislava");
+        hlavneMesta.put("Cesko", "Praha");
+        hlavneMesta.put("Polsko", "Varsava");
+        hlavneMesta.put("Madarsko", "Budapest");
+
+       /* if(hlavneMesta.containsKey("Slovensko")){
+        System.out.println(hlavneMesta.get("Slovensko"));
+          hlavneMesta.remove("Slovensko");
+            System.out.println(hlavneMesta);}*/
+
+        System.out.println(hlavneMesta.size());
+
+        if(hlavneMesta.containsKey("Slovensko")){
+            hlavneMesta.remove("Slovensko");
+        }
+        System.out.println("------");
+        for (String kluc : hlavneMesta.keySet()){
+            System.out.println(kluc);
+            System.out.println(hlavneMesta.get(kluc));
+        }
+        System.out.println("------");
+        for (String hodnota : hlavneMesta.values()){
+            System.out.println(hodnota);
+        }
+        System.out.println("------");
+        System.out.println(hlavneMesta);
+        hlavneMesta.clear();
+        System.out.println(hlavneMesta);
+
         }
     }
 
-    private static void kontrola(Zviera zver) throws NotPesException, NotMackaException {
-        if (zver instanceof Pes){
-            throw new NotMackaException("Nie je to macka");
-        } else {
-            throw new NotPesException("Nie je to pes");
-        }
-    }
-}
+
