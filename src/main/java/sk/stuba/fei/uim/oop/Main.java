bf8b8babccb24d8db8bd43b6c363b98d18f3c16b
MichalZakopcanik <xzakopcanik@stuba.sk>
2022-03-17 22:13:06 +0100

    set

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -3,45 +3,32 @@ package sk.stuba.fei.uim.oop;
 
 import sk.stuba.fei.uim.oop.zvierata.*;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         List<Zviera> zveri = new ArrayList<>(List.of(new Pes("Dunco"), new Macka("Murko")));
 
-        Map<String, String> hlavneMesta = new HashMap<>();
-        hlavneMesta.put("Slovensko", "Bratislava");
-        hlavneMesta.put("Cesko", "Praha");
-        hlavneMesta.put("Polsko", "Varsava");
-        hlavneMesta.put("Madarsko", "Budapest");
+        Set<String> auta = new HashSet<>();
+        auta.add("Volvo");
+        auta.add("BMW");
+        auta.add("VM");
+        auta.add("Audi");
+        auta.add("Volvo");
+        auta.add("BMW");
 
-       /* if(hlavneMesta.containsKey("Slovensko")){
-        System.out.println(hlavneMesta.get("Slovensko"));
-          hlavneMesta.remove("Slovensko");
-            System.out.println(hlavneMesta);}*/
+        System.out.println(auta);
+        System.out.println(auta.size());
 
-        System.out.println(hlavneMesta.size());
-
-        if(hlavneMesta.containsKey("Slovensko")){
-            hlavneMesta.remove("Slovensko");
+        if(auta.contains("Volvo")){
+            auta.remove("Volvo");
         }
-        System.out.println("------");
-        for (String kluc : hlavneMesta.keySet()){
-            System.out.println(kluc);
-            System.out.println(hlavneMesta.get(kluc));
-        }
-        System.out.println("------");
-        for (String hodnota : hlavneMesta.values()){
-            System.out.println(hodnota);
-        }
-        System.out.println("------");
-        System.out.println(hlavneMesta);
-        hlavneMesta.clear();
-        System.out.println(hlavneMesta);
 
+        List<String> auta2 = new ArrayList<>(List.of("Volvo", "BMW"));
+        System.out.println(auta);
+        auta.removeAll(auta2);
+        System.out.println(auta);
+        auta.clear();
         }
     }
 
